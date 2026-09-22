ddab051ef1ca2003e2f988d689782a7b663e57b9
usmonow-dotcom <usmonow@gmail.com>
2026-09-22 17:43:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,15 @@ bot = telebot.TeleBot("8970808690:AAHpSXLaDk1knzJ3mnMKFuJETFfqEAm8bqs")
 
 @bot.message_handler(content_types=['video'])
 def handle_video(message):
-    bot.reply_to(message, "🎥 Видео получил! Готов обработать.")
+    bot.reply_to(message, "⏳ Скачиваю видео...")
+
+    file_info = bot.get_file(message.video.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open("video.mp4", "wb") as f:
+        f.write(downloaded_file)
+
+    bot.send_video(message.chat.id, open("video.mp4", "rb"))
 
 @bot.message_handler(func=lambda message: True)
 def reply(message):
