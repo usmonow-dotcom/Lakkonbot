0e1dee889e7bb0b75309eef5ad9d219dbedbc9cc
usmonow-dotcom <usmonow@gmail.com>
2026-09-22 15:39:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,12 @@ import telebot
 
 bot = telebot.TeleBot("8970808690:AAHpSXLaDk1knzJ3mnMKFuJETFfqEAm8bqs")
 
+@bot.message_handler(content_types=['video'])
+def handle_video(message):
+    bot.reply_to(message, "🎥 Видео получил! Готов обработать.")
+
 @bot.message_handler(func=lambda message: True)
 def reply(message):
-    bot.reply_to(message, "Привет! Я работаю.")
+    bot.reply_to(message, "Привет! Я работаю. Отправь мне видео 🎥")
 
-bot.infinity_polling() 
+bot.infinity_polling()
